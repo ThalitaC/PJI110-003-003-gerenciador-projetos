d35c024a058e647edcef869c55708d674c6adb17
ThalitaC <thalitaceci@gmail.com>
2024-05-02 20:55:39 -0300

    fix module importers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientesModule } from './clientes/clientes.module';
-import { ProjetosModule } from './projetos/projetos.module';
-import { TarefasModule } from './tarefas/tarefas.module';
 
 @Module({
-  imports: [ClientesModule, ProjetosModule, TarefasModule],
   controllers: [AppController],
   providers: [AppService],
 })
