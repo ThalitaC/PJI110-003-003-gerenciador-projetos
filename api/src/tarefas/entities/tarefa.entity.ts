99389b7bf5188c797a32268d297d06129ec3603d
ThalitaC <thalita.cecilier@priceline.com>
2024-05-04 12:16:41 -0300

    Configura typeorm/entidades e conexao com postgres

--- api/src/tarefas/entities/tarefa.entity.ts
+++ api/src/tarefas/entities/tarefa.entity.ts
@@ -1,11 +1,36 @@
-import {Entity, PrimaryColumn} from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Projeto } from '../../projetos/entities/projeto.entity';
+import { Cliente } from '../../clientes/entities/cliente.entity';
 
 @Entity('tarefas')
 export class Tarefa {
-    @PrimaryColumn()
-    id: string
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    nome: string
-    duracao: number
-    projetoId: string
+  @ManyToOne(() => Projeto, (projeto) => projeto.tarefas)
+  @JoinColumn({ name: 'projeto_id' })
+  projeto: Projeto;
+
+  @Column({ name: 'tarefa_nome', type: 'varchar', nullable: false })
+  nome: string;
+
+  @Column({ name: 'tarefa_duracao', type: 'interval', nullable: false })
+  duracao: number;
+
+  @Column({ name: 'tarefa_semana', type: 'integer', nullable: false })
+  semana: number;
+
+  @ManyToOne(() => Cliente, (cliente) => cliente.projetos)
+  @JoinColumn({ name: 'cliente_id' })
+  cliente: Cliente;
+
+  @OneToMany(() => Tarefa, (tarefa) => tarefa.projeto)
+  tarefas: Tarefa[];
 }
