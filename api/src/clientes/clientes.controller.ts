260af234acaceae1361b369c3ef2cd0b9eab1c98
ThalitaC <thalita.cecilier@priceline.com>
2024-05-05 10:58:22 -0300

    Refatora criaçao de clientes, adiciona validaçoes

--- api/src/clientes/clientes.controller.ts
+++ api/src/clientes/clientes.controller.ts
@@ -7,23 +7,45 @@ import {
   Param,
   Delete,
   Query,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { ClientesService } from './clientes.service';
-import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
 import { Cliente } from './entities/cliente.entity';
+import { CreateClienteDto } from './dto/create-cliente.dto';
 
 @Controller('clientes')
 export class ClientesController {
   constructor(private readonly clientesService: ClientesService) {}
 
   @Post()
-  create(
-    @Query('nome') nome: string,
-    @Body() createClienteDto?: CreateClienteDto,
+  async create(
+    @Query() queryParams: CreateClienteDto,
+    @Res() res?: Response,
   ): Promise<Cliente> {
-    const { email, telefone, cnpj } = createClienteDto || {};
-    return this.clientesService.create(nome, email, telefone, cnpj);
+    try {
+      const { nome = '', email = '', telefone = '', cnpj = '' } = queryParams;
+      const novoCliente = await this.clientesService.create(
+        nome,
+        email || '',
+        telefone || '',
+        cnpj || '',
+      );
+      res.status(201).json(novoCliente);
+      return novoCliente;
+    } catch (error) {
+      if (error.message === 'Nome não pode ser vazio') {
+        res.status(400).json({ error: 'Nome não pode ser vazio' });
+      } else if (error.message === 'CNPJ já cadastrado') {
+        res.status(409).json({ error: 'CNPJ já cadastrado' });
+      } else if (error.message === 'CNPJ pode conter apenas números') {
+        res.status(400).json({ error: 'CNPJ pode conter apenas números' });
+      } else {
+        res.status(500).json({ error: 'Erro ao criar cliente' });
+      }
+      throw error;
+    }
   }
 
   @Get()
