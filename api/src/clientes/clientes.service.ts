727b8c11c9ec98b2296761baa1867fb418bd3758
ThalitaC <thalita.cecilier@priceline.com>
2024-05-05 17:07:22 -0300

    Adiciona update e delete Clientes

--- api/src/clientes/clientes.service.ts
+++ api/src/clientes/clientes.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cliente } from './entities/cliente.entity';
-import { UpdateClienteDto } from './dto/update-cliente.dto';
 import {
-  NOME_VAZIO,
+  CLIENTE_NAO_ENCONTRADO,
   CNPJ_EXISTENTE,
   CNPJ_NAO_NUMEROS,
-  NENHUM_CLIENTE_ENCONTRADO,
-  CLIENTE_NAO_ENCONTRADO,
   ID_INVALIDO,
+  NENHUM_CLIENTE_ENCONTRADO,
+  NOME_VAZIO,
 } from '../erros/erros';
 
 @Injectable()
@@ -25,23 +24,7 @@ export class ClientesService {
     telefone?: string,
     cnpj?: string,
   ): Promise<Cliente> {
-    if (!nome) {
-      throw new Error(NOME_VAZIO);
-    }
-
-    const cnpjContemApenasNumeros = /^\d+$/.test(cnpj || '');
-
-    if (cnpj && !cnpjContemApenasNumeros) {
-      throw new Error(CNPJ_NAO_NUMEROS);
-    }
-
-    const clienteCadastrado = await this.clienteRepository.findOne({
-      where: { cnpj },
-    });
-
-    if (clienteCadastrado && clienteCadastrado.cnpj !== null) {
-      throw new Error(CNPJ_EXISTENTE);
-    }
+    await this.validaNomeCNPJ(nome, cnpj);
 
     const novoCliente = this.clienteRepository.create({
       nome,
@@ -64,6 +47,55 @@ export class ClientesService {
   }
 
   async findOne(id: string): Promise<Cliente> {
+    await this.validaID(id);
+
+    await this.validaClienteExiste(id);
+    return await this.clienteRepository.findOne({ where: { id } });
+  }
+
+  async update(data: {
+    id: string;
+    nome?: string;
+    email?: string;
+    telefone?: string;
+    cnpj?: string;
+  }): Promise<Cliente> {
+    const { id, nome, email, telefone, cnpj } = data;
+    await this.validaID(id);
+    await this.validaClienteExiste(id);
+    await this.validaNomeCNPJ(nome, cnpj);
+
+    await this.clienteRepository.update(
+      { id },
+      { nome, email, telefone, cnpj },
+    );
+
+    return await this.clienteRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async remove(id: string): Promise<void> {
+    try {
+      await this.validaClienteExiste(id);
+      await this.validaID(id);
+      await this.clienteRepository.delete(id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  private async validaClienteExiste(id: string) {
+    const cliente = await this.clienteRepository.findOne({
+      where: { id },
+    });
+    if (!cliente) {
+      throw new Error(CLIENTE_NAO_ENCONTRADO);
+    }
+  }
+
+  private async validaID(id: string) {
     const idUuid = id.match(
       /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
     );
@@ -71,28 +103,25 @@ export class ClientesService {
     if (!idUuid) {
       throw new Error(ID_INVALIDO);
     }
+  }
 
-    const cliente = await this.clienteRepository.findOne({
-      where: { id },
-    });
-
-    if (!cliente) {
-      throw new Error(CLIENTE_NAO_ENCONTRADO);
+  private async validaNomeCNPJ(nome: string, cnpj: string) {
+    if (!nome) {
+      throw new Error(NOME_VAZIO);
     }
-    return cliente;
-  }
 
-  async update(
-    id: string,
-    updateClienteDto: UpdateClienteDto,
-  ): Promise<Cliente> {
-    await this.clienteRepository.update(id, updateClienteDto);
-    return this.clienteRepository.findOne({
-      where: { id },
+    const cnpjContemApenasNumeros = /^\d+$/.test(cnpj || '');
+
+    if (cnpj && !cnpjContemApenasNumeros) {
+      throw new Error(CNPJ_NAO_NUMEROS);
+    }
+
+    const clienteCadastrado = await this.clienteRepository.findOne({
+      where: { cnpj },
     });
-  }
 
-  async remove(id: string): Promise<void> {
-    await this.clienteRepository.delete(id);
+    if (clienteCadastrado && clienteCadastrado.cnpj !== null) {
+      throw new Error(CNPJ_EXISTENTE);
+    }
   }
 }
