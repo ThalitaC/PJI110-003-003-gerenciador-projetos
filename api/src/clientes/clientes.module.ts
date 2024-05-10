10b44d5f93ee9f6ba8daa4b2272f9b0f2301c8db
ThalitaC <thalita.cecilier@priceline.com>
2024-05-10 20:14:56 -0300

    Fix delete Cliente

--- api/src/clientes/clientes.module.ts
+++ api/src/clientes/clientes.module.ts
@@ -3,10 +3,12 @@ import { ClientesService } from './clientes.service';
 import { ClientesController } from './clientes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cliente } from './entities/cliente.entity';
+import { Projeto } from '../projetos/entities/projeto.entity';
+import { ProjetosService } from '../projetos/projetos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cliente])],
+  imports: [TypeOrmModule.forFeature([Cliente, Projeto])],
   controllers: [ClientesController],
-  providers: [ClientesService],
+  providers: [ClientesService, ProjetosService],
 })
 export class ClientesModule {}
